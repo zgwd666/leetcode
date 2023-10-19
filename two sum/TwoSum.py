98a0fdd92584fc431aa1efe503956ec3453fd844
chenzhi <65932381+zgwd666@users.noreply.github.com>
2023-10-19 16:40:10 +0800

    Update TwoSum.py

--- two sum/TwoSum.py
+++ two sum/TwoSum.py
@@ -1,1 +1,12 @@
-
+#https://leetcode.cn/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        self.num=nums
+        self.target=target
+        for i in range(len(self.num)):
+            for j in range(i+1,len(self.num)):
+                if self.num[i]+self.num[j]==self.target:
+                    return [i,j]
+                j+=1
+            i+=1
+        return [i,j]
