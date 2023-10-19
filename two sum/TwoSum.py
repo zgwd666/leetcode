73831ebd5c2d45360920b93fa9612d0d841d4b42
chenzhi <65932381+zgwd666@users.noreply.github.com>
2023-10-19 16:45:39 +0800

    Update TwoSum.py

--- two sum/TwoSum.py
+++ two sum/TwoSum.py
@@ -1,8 +1,16 @@
 #https://leetcode.cn/problems/two-sum/
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        输入：
+        nums:输入的数组
+        target：目标值
+        输出：
+        [i,j]：满足加和等于目标值的数组下标
+        '''
         self.num=nums
         self.target=target
+        #进行循环遍历
         for i in range(len(self.num)):
             for j in range(i+1,len(self.num)):
                 if self.num[i]+self.num[j]==self.target:
