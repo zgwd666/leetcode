3b9fc093d718fde4292ea86da8c135c19aa59d4e
chenzhi <65932381+zgwd666@users.noreply.github.com>
2023-10-19 21:43:55 +0800

    Update ValidParentheses.py

--- Valid Parentheses/ValidParentheses.py
+++ Valid Parentheses/ValidParentheses.py
@@ -1,1 +1,19 @@
-
+#https://leetcode.cn/problems/valid-parentheses/description/
+class Solution:
+    def isValid(self, s: str) -> bool:
+        length=len(s)
+        str=list(s)
+        dict_Parentheses={'(':')','{':'}','[':']'}
+        if length%2!=0:
+            return False
+        else:
+            stack=[]
+            for i in str:
+                if i in dict_Parentheses:
+                    stack.append(i)
+                elif stack!=[]:
+                    if dict_Parentheses[stack.pop()]!=i:
+                        return False
+                else :
+                    return False
+            return len(stack)==0
