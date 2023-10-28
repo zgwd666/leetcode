d115b40c0bc03de217529de2f29022d15466d752
chenzhi <65932381+zgwd666@users.noreply.github.com>
2023-10-28 15:21:07 +0800

    Update 3Sum Closest.py

--- 3Sum Closest/3Sum Closest.py
+++ 3Sum Closest/3Sum Closest.py
@@ -1,1 +1,33 @@
+#https://leetcode.cn/problems/3sum-closest/description/
+'''
+解题思路：
+采用双指针的思路。
 
+'''
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        if len(nums)<=3:
+            return sum(nums)
+        res=0
+        min_dist=1e6
+        nums=sorted(nums)
+        for i in range(len(nums)):
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                s=nums[i]+nums[l]+nums[r]
+                cur_dist=s-target
+                if cur_dist==0:
+                    return s
+                elif cur_dist<0 :
+                    l+=1
+                    if abs(cur_dist)<min_dist:
+                        min_dist=abs(cur_dist)
+                        res=s
+                else:
+                    r-=1
+                    if cur_dist<min_dist:
+                        min_dist=cur_dist
+                        res=s
+        return res
+                    
