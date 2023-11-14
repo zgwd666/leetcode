19c74943e217e38ac668e4d3e21095a18b3f0bec
chenzhi <65932381+zgwd666@users.noreply.github.com>
2023-11-14 16:47:02 +0800

    Update Maximum Subarray.py

--- Maximum Subarray/Maximum Subarray.py
+++ Maximum Subarray/Maximum Subarray.py
@@ -1,1 +1,18 @@
-
+#https://leetcode.cn/problems/maximum-subarray/description/
+'''
+解题思路：采用动态规划
+设最优解dp[i]:
+  如果dp[i-1]<=0,dp[i]=nums[i]
+  如果dp[i-1]>0,dp[i]=dp[i-1]+nums[i]
+初始状态dp[0]=num[0]
+'''
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp=[0]*len(nums)#创建状态转移数组
+        dp[0]=nums[0]#初始化状态
+        for i in range(1,len(nums)):#进行遍历  如果dp[i-1]<=0,dp[i]=nums[i]  如果dp[i-1]>0,dp[i]=dp[i-1]+nums[i]
+            if dp[i-1]>0:
+                dp[i]=dp[i-1]+nums[i]
+            else:
+                dp[i]=nums[i]
+        return max(dp)#返回数组中最大值
