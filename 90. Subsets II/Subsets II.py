9e1dac77982d777a26e5537e384f3ec600100b6d
chenzhi <65932381+zgwd666@users.noreply.github.com>
2024-04-20 18:25:05 +0800

    Create Subsets II.py

---
+++ 90. Subsets II/Subsets II.py
@@ -0,0 +1,27 @@
+#https://leetcode.cn/problems/subsets-ii/
+'''
+解题思路：这个算法利用了回溯法的思想，通过递归和回溯来生成所有可能的子集。它首先尝试添加当前索引的元素，并跳过重复的元素，然后递归地尝试所有后续索引的元素，直到所有可能的组合都被探索完毕。
+'''
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        #result被初始化为一个包含空列表的列表，因为空列表也是一个有效的子集
+        result=[[]]
+        #nums.sort()对输入的列表nums进行排序。排序是生成所有子集的一个常用步骤，因为它可以简化子集的生成过程。
+        nums.sort()
+         #dfs是一个深度优先搜索（Depth-First Search, DFS）的递归函数，用于生成所有可能的子集。
+        def dfs(nums,index,path,result):
+            #如果path非空，那么它代表了一个有效的子集，将其添加到result中
+            if path:
+                result.append(path[:])
+                   #在每次迭代中，将当前索引的数字添加到path中，然后递归调用dfs函数，递归的下一个索引设置为当前索引加1。
+            for i in range(index,len(nums)):
+                #跳过重复的元素，可以防止出现重复的子集
+                if i>index and nums[i]==nums[i-1]:
+                    continue
+                path.append(nums[i])
+                dfs(nums,i+1,path,result)
+                 #在递归调用之后，执行path.pop()以移除path中的最后一个元素，这是回溯过程的一部分，它允许函数返回到上一个状态并继续探索其他可能的子集。
+                path.pop()
+         #在dfs函数定义之后，使用dfs(nums, 0, [], result)开始递归过程，从索引0开始，初始路径为空。
+        dfs(nums,0,[],result)
+        return result
